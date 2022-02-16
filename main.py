f270dafc677c72f7eda473d6f10a557d5cc7d353
KaleryVrasnov <99810925+KaleryVrasnov@users.noreply.github.com>
2022-02-17 01:20:30 +0300

    Add files via upload
    Lab 1

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-sides = [3, 2, 4, 7, 5, 12, 11, 13, 15, 16, 14, 14]
-sides = sorted(sides, reverse=True)
-smax = 0
-for i in range(len(sides)):
-    for j in range(i + 1, len(sides)):
-        for k in range(j + 1, len(sides)):
-            a = sides[i]
-            b = sides[j]
-            c = sides[k]
-            if a + b > c and a + c > b and b + c > a:
-                p = (a + b + c) / 2
-                s = (p * (p - a) * (p - b) * (p - c)) ** (1 / 2)
-                if s > smax:
-                    smax = s
-print("Максимальная площадь треугольника:", smax)
+a = int(input())
+b = int(input())
+c = int(input())
+d = b * b - 4 * a * c
+if d > 0:
+    x1 = (-b + d ** 0.5) / (2 * a)
+    x2 = (-b - d ** 0.5) / (2 * a)
+    print('Корни уравнения:', x1, ',', x2)
+elif d == 0:
+    x = -b / 2 * a
+    print('Корень уравнения: ', x)
+else:
+    print('Уравнение не имеет корней, дискриминант меньше нуля')
+
+
+
